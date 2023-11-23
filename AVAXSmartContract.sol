2f96ca8806985afc05bb1dd7c79cdc1999b9f87b
Zeno Donato <116099247+Zerneasx@users.noreply.github.com>
2023-11-23 23:59:13 +0800

    Update AVAXSmartContract.sol

--- AVAXSmartContract.sol
+++ AVAXSmartContract.sol
@@ -1,29 +1,70 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract SimpleWallet {
-    address public owner;
-    uint256 public balance;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-    constructor() {
-        owner = msg.sender;
+contract MyToken is ERC20, Ownable {
+    uint256 private _maxSupply;
+
+    struct Item {
+        string name;
+        uint256 price;
     }
 
-    function deposit(uint256 amount) public payable {
-        uint256 newBalance = balance + amount;
-        assert(newBalance == 1000); 
-        balance = newBalance;
+    mapping(string => Item) private _items;
+
+    event Mint(address indexed to, uint256 amount);
+    event Burn(address indexed burner, uint256 amount);
+    event Buy(address indexed buyer, string item, uint256 price);
+
+    constructor(
+        string memory name,
+        string memory symbol,
+        uint256 initialSupply,
+        uint256 maxSupply
+    ) ERC20(name, symbol) {
+        require(initialSupply <= maxSupply, "Initial supply must be less than or equal to max supply");
+        _mint(msg.sender, initialSupply);
+        _maxSupply = maxSupply;
+
+        _items["Cable"] = Item("Cable", 20);
+        _items["Battery"] = Item("Battery", 150);
+        _items["Phone"] = Item("Phone", 250);
     }
 
-    function withdraw(uint256 amount) public {
-        require(amount > 0, "The amount must be greater than 0");
-        require(balance >= amount, "Insufficient funds");
-        if (amount <= 250) {
-            revert("Revert condition: amount should not be less than or equal to 250");
+    function mint(address to, uint256 amount) public onlyOwner {
+        require(totalSupply() + amount <= _maxSupply, "Exceeds maximum supply");
+        _mint(to, amount);
+        emit Mint(to, amount);
+    }
+
+    function burn(uint256 amount) public {
+        _burn(msg.sender, amount);
+        emit Burn(msg.sender, amount);
+    }
+
+    function transfer(address to, uint256 value) public override returns (bool) {
+        require(totalSupply() + value <= _maxSupply, "Exceeds maximum supply");
+        return super.transfer(to, value);
+    }
+
+    function buy(string memory itemName) public {
+        Item memory item = _items[itemName];
+        require(bytes(item.name).length > 0, "Item not found");
+        require(balanceOf(msg.sender) >= item.price, "Insufficient balance");
+        assert(msg.sender != owner());   
+
+        if (keccak256(abi.encodePacked(itemName)) == keccak256(abi.encodePacked("Cable"))) {
+            burn(item.price);
+        } else if (keccak256(abi.encodePacked(itemName)) == keccak256(abi.encodePacked("Battery"))) {
+            burn(item.price);
+        } else if (keccak256(abi.encodePacked(itemName)) == keccak256(abi.encodePacked("Phone"))) {
+            burn(item.price);
+        } else {
+            revert("Invalid item");
         }
 
-        uint256 newBalance = balance - amount;
-        assert(newBalance <= balance); 
-        balance = newBalance;
+        emit Buy(msg.sender, itemName, item.price);
     }
 }
